de7edfba7485819ee2dc89fd9e1d264894ab9308
Macbook <makich.markosyan@bk.ru>
2021-09-15 02:19:39 +0400

    Initial commit

---
+++ src/Home5.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Home5 {
+
+
+    public static void main(String[] args) {
+//        // Task 1
+        Scanner NumScanner = new Scanner(System.in);
+        System.out.println();
+        System.out.println("Enter any number to start the robot program :) ");
+       int fix1 = NumScanner.nextInt();
+        {
+            int f = 100;
+            while (f >= 1) {
+                System.out.println(f);
+                f--;
+                System.out.println("Run Scanner" + fix1);
+
+            }
+        }
+//        // Task 2
+//        System.out.println();
+//        System.out.println("Enter any number to start the robot program :) ");
+//        int fix1 = NumScanner.nextInt();
+//        for (int u = 16;u >= 1; u-- ) {
+//        if ( u % 3 == 0 ) {
+//            continue;
+//        }else
+//            System.out.println("Number :" + u );
+//        }
+//  }
+        // Task 3
+//        int num, sum = 0;
+//        for (int i = 0; i < 5; i++) {
+//            num = (int) (Math.random() * 10);
+//            System.out.println(num);
+//            sum += num;
+//        }
+//        System.out.println("Sum=" + sum);
+
+// Task 4
+//        int from = 5, to = 10, randomNum;
+//        randomNum = from + (int) (Math.random() * ((to - from) + 1));
+//        System.out.println("Random number is " + randomNum);
+//        for (int i = 1; i < randomNum; i++) {
+//            System.out.println("The cube of " + i + " = " + i * i * i);
+//        }
+    }
+
+
+}
+
